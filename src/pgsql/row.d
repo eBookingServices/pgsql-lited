73e069fda74b4dc58485784bf8bb74d2207a6fd8
Márcio Martins <marcioapm@gmail.com>
2018-08-24 16:35:05 +0200

    Once again

--- src/pgsql/row.d
+++ src/pgsql/row.d
@@ -148,7 +148,7 @@ struct PgSQLRow {
 	}
 
 package:
-	ref auto dispatchFast_(uint hash, string key) {
+	ref auto dispatchFast_(uint hash, string key) const {
 		if (auto index = find_(hash, key))
 			return opIndex(index - 1);
 		throw new PgSQLErrorException("Column '" ~ key ~ "' was not found in this result set");
